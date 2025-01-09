a58152f7f5ba70cd424ea7d0cffaff39a3ed2a75
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-01-09 18:58:05 +0000

    Click number to edit feature working

--- app/components/result/number-display.tsx
+++ app/components/result/number-display.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from "react";
+"use client";
+import { ChangeEvent, HTMLProps, useEffect, useRef, useState } from "react";
 
 type NumberDisplayProps = {
   value: number;
@@ -7,12 +8,49 @@ type NumberDisplayProps = {
 export default function NumberDisplay({ value }: NumberDisplayProps) {
   const [number, setNumber] = useState(value);
   const [isEditing, setIsEditing] = useState(false);
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleNumberClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value, 10);
+    if (!isNaN(newValue)) {
+      setNumber(newValue);
+    }
+  };
+
+  const handleInputBlur = () => setIsEditing(false);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      if (isEditing) {
+        inputRef.current.focus();
+      }
+    }
+  }, [isEditing]);
 
   return (
     <article className="gradient-circle flex flex-col justify-center w-[140px] h-[140px] rounded-full">
       <span className="font-extrabold text-white text-[3.5rem] leading-[4.5rem]">
-        {value}
+        {!isEditing ? (
+          <span
+            className="cursor-pointer select-none"
+            onClick={handleNumberClick}
+          >
+            {number}
+          </span>
+        ) : (
+          <input
+            type="number"
+            ref={inputRef}
+            value={number}
+            onChange={handleInputChange}
+            onBlur={handleInputBlur}
+            className="w-full outline-none focus:ring focus:ring-blue-300 rounded-3xl text-indigo-700 bg-indigo-100 text-center"
+          />
+        )}
       </span>
       <span className="text-fig-light-blue font-bold">of 100</span>
     </article>
